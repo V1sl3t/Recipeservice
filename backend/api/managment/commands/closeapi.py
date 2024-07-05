01d71a34b2af606813702eab17d4b9965b14b22a
Степан Суппес <suppes214@gmail.com>
2024-07-05 20:07:11 +0300

    1

---
+++ backend/api/managment/commands/closeapi.py
@@ -0,0 +1,23 @@
+import csv
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from recipes.models import (Ingredient, Tag)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        data = [
+            {'name': 'Завтрак', 'slug': 'breakfast'},
+            {'name': 'Обед', 'slug': 'lunch'},
+            {'name': 'Ужин', 'slug': 'dinner'}]
+        Tag.objects.bulk_create(Tag(**tag) for tag in data)
+        with open(
+            f'{settings.BASE_DIR}/data/ingridients.csv',
+            'r',
+            encoding='utf-8'
+        ) as file:
+            Ingredient.objects.bulk_create(
+                Ingredient(**data) for data in csv.DictReader(file))
