6cf91a46a88292f8a7010804d153636be6987062
Степан Суппес <suppes214@gmail.com>
2024-07-09 18:31:39 +0300

    4

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -71,9 +71,9 @@ class UserSubscribtionGetSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('email', 'id', 'username', 'first_name',
-                  'last_name', 'is_subscribed', 'recipes', 'recipes_count')
+                  'last_name', 'recipes', 'recipes_count')
         read_only_fields = ('email', 'username', 'first_name', 'last_name',
-                            'is_subscribed', 'recipes', 'recipes_count')
+                            'recipes', 'recipes_count')
 
     def get_recipes(self, obj):
         request = self.context.get('request')
