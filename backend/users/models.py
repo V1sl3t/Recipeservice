9078e3bf6b9746898202df3f927347d45babfbc4
Степан Суппес <suppes214@gmail.com>
2024-07-11 06:48:10 +0300

    fin

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser
 
 from foodgram_backend import constants
 
