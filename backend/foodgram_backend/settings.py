0e32136e13852570b6ecc4997aef64ea029f8d53
Степан Суппес <suppes214@gmail.com>
2024-07-11 11:06:07 +0300

    eee

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -59,11 +59,16 @@ WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
