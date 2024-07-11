253bf2d2df01a09452d4f421993a3d594adccd04
Степан Суппес <suppes214@gmail.com>
2024-07-11 14:06:38 +0300

    eee

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -22,12 +22,12 @@ class RecipeFilter(FilterSet):
 
     def get_is_favorited(self, queryset, name, value):
         if value:
-            return queryset.filter(favorites__user=self.request.user)
+            return queryset.filter(favorites__user__id=self.request.user.id)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         if value:
-            return queryset.filter(carts__user=self.request.user)
+            return queryset.filter(carts__user__id=self.request.user.id)
         return queryset
 
 
