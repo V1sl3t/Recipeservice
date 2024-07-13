8964c4aabbfb24fa8c4ddc4bdf1c60e7efb0dc9a
Степан Суппес <suppes214@gmail.com>
2024-07-13 10:54:02 +0300

    4

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -5,7 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SEKERET_KEY', '1')
 
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = True
+# DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost').split(', ')
 
@@ -58,13 +59,9 @@ WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
